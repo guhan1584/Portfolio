505d340bdce5d22fd42a400e6a19fdf106873e9d
guhan1584 <guhan4321@gmail.com>
2024-09-24 18:16:06 +0300

    Intial components

---
+++ Personal-Website/src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section id="home" className="hero">
+      <div className="hero-content">
+        <h1>Hello, I'm [Your Name]</h1>
+        <p>Frontend Developer | JavaScript Enthusiast | UI/UX Designer</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;

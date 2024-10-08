020716035f9858528c0dcbf59068f47534368c78
guhan1584 <guhan4321@gmail.com>
2024-10-08 18:00:19 +0300

    Theme

--- Personal-Website/src/main.tsx
+++ Personal-Website/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
 import theme from "./Theme.tsx";
@@ -8,6 +8,7 @@ import theme from "./Theme.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
   </StrictMode>

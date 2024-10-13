8e91538615814d60c214eb4f8faf7b82e9148065
guhan1584 <guhan4321@gmail.com>
2024-10-13 11:57:31 +0300

    Making the Footer in the bottom of the pages

--- Personal-Website/src/pages/Layout.tsx
+++ Personal-Website/src/pages/Layout.tsx
@@ -1,16 +1,19 @@
 import NavBar from "../components/NavBar";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
     <>
-      <NavBar />
-      <Box>
-        <Outlet />
-      </Box>
-      <Footer />
+      <Flex direction="column" minHeight="100vh">
+        <NavBar />
+        {/* Flex-grow here ensures the main content takes up remaining space */}
+        <Box flex="1">
+          <Outlet />
+        </Box>
+        <Footer />
+      </Flex>
     </>
   );
 };
